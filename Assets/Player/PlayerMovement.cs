1f7bca92e5624e8a8c8439bd309f0230e55124ce
U-BABY\Cakeydoodoo <tvu010@campus.goldsmiths.ac.uk>
2018-02-06 20:14:23 +0000

    added animations for movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -6,16 +6,20 @@ public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed;
     public float rotationSpeed;
+    public float runSpeed;
+    public float speed;
+    public float jumpForce;
 
+    static Animator anim;
     Rigidbody rb;
     Camera myCamera;
-    public float jumpForce;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         myCamera = Camera.main;
+        anim = GetComponent<Animator>();
     }
 
     void Start () {
@@ -31,16 +35,80 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jump();
+
         }
+
+
 
     }
 
     void Movement()
     {
+
+        //set players speed
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+
+        }else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = movementSpeed;
+        }
+
+        /*
+        //run
+        if (Input.GetKeyDown(KeyCode.LeftShift) | Input.GetKeyDown("w"))
+        {
+            anim.SetBool("walkRun",true);
+            anim.SetBool("idleWalk", false);
+        }//idle
+        else if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp("w"))
+        {
+            anim.SetBool("idleRun", false);
+        }
+        // walk
+        else if (Input.GetKeyUp(KeyCode.LeftShift) | Input.GetKeyDown("w"))
+        {
+            anim.SetBool("idleWalk",true);
+        }//idle
+        else if (Input.GetKeyUp(KeyCode.LeftShift) | Input.GetKeyUp("w"))
+        {
+            anim.SetBool("idleWalk", false);
+            anim.SetBool("walkRun", false);
+
+        }
+        */  
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) | Input.GetKeyUp("w"))
+        {
+            anim.SetBool("idleWalk", false);
+            anim.SetBool("walkRun", false);
+        } else if(Input.GetKeyUp(KeyCode.LeftShift) | Input.GetKeyDown("w"))
+        {
+            anim.SetBool("idleWalk", true);
+            anim.SetBool("walkRun", false);
+        }//running
+        else if (Input.GetKeyDown(KeyCode.LeftShift) | Input.GetKeyDown("w"))
+        {
+            anim.SetBool("walkRun", true);
+            //anim.SetBool("idleWalk", false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) | Input.GetKeyDown("w"))
+        {
+            anim.SetBool("idleWalk", true);
+            anim.SetBool("walkRun", false);
+        }
+
+
+
         //player movement
         Vector3 inputVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        inputVelocity = inputVelocity * movementSpeed * Time.deltaTime;
+        inputVelocity = inputVelocity * speed * Time.deltaTime;
         rb.MovePosition(transform.position + (transform.forward * inputVelocity.z) + (transform.right * inputVelocity.x));
+
+
+
 
 
         //camera rotation
@@ -56,6 +124,7 @@ public class PlayerMovement : MonoBehaviour {
     void jump()
     {
         rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        anim.SetTrigger("jump");
 
     }
 
