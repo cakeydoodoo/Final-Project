7688e399cc457ec0b84916228d0d29d40a11ea4c
U-BABY\Cakeydoodoo <tvu010@campus.goldsmiths.ac.uk>
2018-03-19 16:42:13 +0000

    logs

---
+++ Assets/enemy/enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class enemy : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    NavMeshAgent nav;
+    public Transform target;
+
+
+    // Enemy Health
+    public int enemyHealth;
+    public int currentHealth;
+
+    public int damage;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //  rb = GetComponent<Rigidbody>();
+        nav = GetComponent<NavMeshAgent>();
+
+
+
+    }
+
+    void Awake()
+    {
+
+
+    }
+
+    // Update is called once per frame
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        enemyMovement();
+
+
+    }
+
+    void enemyMovement()
+    {
+
+        nav.SetDestination(target.position);
+
+    }
+
+
+    void die()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
