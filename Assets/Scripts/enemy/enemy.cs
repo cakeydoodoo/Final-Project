3a94aa60de8a0fc39a2ead6bed210b7bf9249c9d
U-LIFE\cakeydoodoo <cakeydoodoo>
2018-05-01 00:34:09 +0100

    environement added, need to do the AI

---
+++ Assets/Scripts/enemy/enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+
+public class enemy : MonoBehaviour
+{
+    GameObject player;
+    PlayerUI playerUI;
+    Rigidbody rb;
+
+    NavMeshAgent nav;
+    Transform target; 
+
+
+    // Enemy Health
+    public int enemyHealth;
+    public int currentHealth;
+    public int damage;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //  rb = GetComponent<Rigidbody>();
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("player").transform;
+        playerUI = GameObject.Find("player").GetComponent<PlayerUI>();
+        player = GameObject.Find("player1");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyMovement();
+    }
+
+    void enemyMovement()
+    {
+        //sets the target of the enemy to the players position, following the player
+        nav.SetDestination(target.position);
+    }
+
+
+    void die()
+    {
+        UIManager.scoreNumber += 1;
+        Destroy(this.gameObject);
+        //destroys the game object and then adds 1 to the players score
+    }
+
+    //FIX THIS, IT DESTROYS THE ENEMIES WHEN THEY SPAWN
+    void OnCollisionEnter(Collision other)
+    {
+        if (//other.gameObject.GetComponent<enemy>()
+            other.gameObject == player
+            )
+        {
+            playerUI.TakeDamage(10);
+            other.gameObject.SendMessage("die");
+        }
+        //Destroy(this.gameObject);
+
+    }
+
+}
---
+++ Assets/Scripts/player/PlayerUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerUI : MonoBehaviour {
+    public int maxHealth;
+    public int currentHealth;
+    public Slider healthBar;
+    PlayerMovement playerMovementRef;
+    Animator anim;
+
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    bool damaged;
+    bool dead;
+
+	// Use this for initialization
+
+	void Start ()
+    {
+        anim = GameObject.Find("player").GetComponent<Animator>();  
+        playerMovementRef = GetComponent<PlayerMovement>();
+        currentHealth = maxHealth;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        // Reset the damaged flag.
+        damaged = false;
+    
+		
+	}
+
+    public void TakeDamage(int damage)
+    {
+        damaged = true;
+
+        currentHealth -= damage;
+
+        healthBar.value = currentHealth;
+
+        if (currentHealth <= 0 && !dead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        dead = true;
+        playerMovementRef.enabled = false;
+    }
+}
